d39379fca15d3c9cd3b4ce62cb3c667c6ed9326a
Ytz7 <1970026596@qq.com>
2022-08-08 11:28:21 +0800

    异常处理类

--- common/service-base/src/main/java/com/atguigu/servicebase/handler/MyMetaObjectHandler.java
+++ common/service-base/src/main/java/com/atguigu/servicebase/handler/MyMetaObjectHandler.java
@@ -1,4 +1,4 @@
-package com.atguigu.servicebase.config.handler;
+package com.atguigu.servicebase.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
